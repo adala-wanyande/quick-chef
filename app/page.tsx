4ace6ba1438d7b82a4825a19acfe5facbc997be2
adala-wanyande <benardwanyande@gmail.com>
2024-04-06 08:44:07 +0300

    feat: implement complete problem/solution section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import NavigationBar from "./components/NavigationBar";
 import Hero from "./components/Hero";
+import ProblemSolution from "./components/ProblemSolution";
 
 export default function Home() {
   return (
     <>
         <NavigationBar />
         <Hero />
+        <ProblemSolution />
     </>
   );
 }
