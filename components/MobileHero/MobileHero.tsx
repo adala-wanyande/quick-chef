df0d55b6c76ccc74bd51c758ce00dee17693e2e8
adala-wanyande <benardwanyande@gmail.com>
2024-04-15 13:38:07 +0300

    style: implement fully responsive hero section

--- components/MobileHero/MobileHero.tsx
+++ components/MobileHero/MobileHero.tsx
@@ -8,7 +8,7 @@ import Typography from "@mui/material/Typography";
 import Image from "next/image";
 import { ArrowForward } from "@mui/icons-material";
 
-export default function Hero() {
+export default function MobileHero() {
   return (
     <Box
       id="hero"
@@ -17,87 +17,82 @@ export default function Hero() {
         backgroundImage: "linear-gradient(180deg, #CEE5FD, #FFF)",
         backgroundSize: "100% 20%",
         backgroundRepeat: "no-repeat",
-        display: { xs: "none", md: "block" },
+        display: { xs: "block", md: "none" },
       }}
     >
       <Container
         sx={{
           display: "flex",
-          width: "100%",
-          flexDirection: { xs: "column", sm: "column", md: "row" },
-          alignItems: "center",
-          marginRight: 4,
-          pt: { xs: 14, sm: 20 },
-          pb: { xs: 8, sm: 12 },
+          flexDirection: "column",
+          alignItems: "left",
+          py: "40px",
+          px: "24px",
         }}
       >
-        <Stack spacing={2} useFlexGap sx={{ width: { xs: "100%", sm: "70%" } }}>
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: "100%" } }}>
           <Typography
             variant="h1"
             sx={{
               display: "flex",
-              flexDirection: { xs: "column", md: "column" },
-              alignSelf: "left",
+              flexDirection: "column",
               textAlign: "left",
-              width: "100%",
-              fontSize: "clamp(3rem, 8vw, 3rem)",
+              fontSize: "36px",
             }}
           >
-            Chef-on-Demand,
+            Chef-on-Demand
             <Typography
               component="span"
               variant="h1"
               sx={{
-                fontSize: "clamp(3rem, 8vw, 3rem)",
+                fontSize: "36px",
                 color: "secondary.main",
               }}
             >
               Gourmet Meals
             </Typography>
-            Crafted in
-            <Typography
-              component="span"
-              variant="h1"
-              sx={{
-                fontSize: "clamp(3rem, 8vw, 3rem)",
-                color: "secondary.main",
-              }}
-            >
-              Your Kitchen
-            </Typography>
           </Typography>
           <Typography
             textAlign="left"
             color="text.secondary"
-            sx={{ alignSelf: "left", width: { sm: "100%", md: "80%" } }}
+            sx={{
+              fontSize: "16px",
+            }}
           >
             Connect instantly with culinary experts, explore unique cuisines,
-            and book a personalized cooking experience. Download now to
-            transform your meals into culinary adventures.
+            and book a personalized cooking experience.
           </Typography>
           <Stack
             direction={{ xs: "column", sm: "row" }}
             alignSelf="left"
             spacing={1}
             useFlexGap
-            sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
+            sx={{ pt: 2, pb: 6 }}
           >
             <Button
               variant="contained"
               color="primary"
+              sx={{ width: "160px" }}
               endIcon={<ArrowForward />}
             >
               Book a Chef
             </Button>
           </Stack>
         </Stack>
-        <Box>
+        <Box
+          sx={{
+            width: "100%", // responsive width
+            height: 0, // responsive height based on width
+            paddingTop: "80%", // aspect ratio
+            position: "relative",
+            borderRadius: "10px",
+            overflow: "hidden",
+          }}
+        >
           <Image
             alt="Landing page image"
             src="/images/hero.png"
-            width="500"
-            height="500"
-            style={{ borderRadius: "40px" }}
+            layout="fill"
+            objectFit="cover"
           />
         </Box>
       </Container>
