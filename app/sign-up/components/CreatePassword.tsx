7412a599b404f07db6fc7a6b6f6cb23bf64480b5
adala-wanyande <benardwanyande@gmail.com>
2024-04-22 10:47:01 +0300

    feat: implemenent create password page

--- app/sign-up/components/CreatePassword.tsx
+++ app/sign-up/components/CreatePassword.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const CreatePassword = () => {
-  return (
-    <div>CreatePassword</div>
-  )
-}
+  const [password, setPassword] = useState("");
 
-export default CreatePassword
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Implement password validation and continuation logic here
+    console.log("Create password:", password);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          paddingTop: { xs: 8, md: 16, lg: 16 },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 2, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Create your account password
+        </Typography>
+        <Typography sx={{ mt: 2, mb: 2 }} color="text.secondary">
+          Your password must be at least 8 characters long, and contain at least
+          one digit and one non-digit character
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1, width: "100%" }}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Enter your password"
+            type="password"
+            name="password"
+            autoComplete="new-password"
+            autoFocus
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Button
+            type="button"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleSubmit}
+            disabled={password.length === 0}
+          >
+            Create Password
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default CreatePassword;
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Email from "./components/Email";
 import VerifyEmail from "./components/VerifyEmail";
 import PhoneNumber from "./components/PhoneNumber";
+import CreatePassword from "./components/CreatePassword";
 
 const SignUp = () => {
   return <>
   <Email />
   <VerifyEmail />
   <PhoneNumber />
+  <CreatePassword />
   </>;
 };
 
